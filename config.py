c8ececbbb43e1414e6f792ea535bb71ed85b48da
blakeadams2355-spec <blakeadams2355@gmail.com>
2026-01-04 15:59:46 +0300

    Добавлен конфиг

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+API_ID = 29882404       # Твой API_ID
+API_HASH = "78d0b7392746aa239403cea9d6bd36a1"   # Твой API_HASH
+BOT_TOKEN = "8419220086:AAEvcyx-N963NzNhSj4F5CIMSqO5chnbwZs"        # Твой BOT_TOKEN
+ADMIN_ID = 8181234841   # Твой ID (число)
